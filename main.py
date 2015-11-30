52a53ac2e135afe5c25dff89bf5dbaae856a7d5f
andreshazard <andres.hazard@servicerocket.com>
2015-11-29 21:55:48 -0300

    add error handler

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import base64
 import json
 import urllib2
 import pprint
+import sys
 
 
 def get_from_api_jira(a_list):
@@ -43,25 +44,29 @@ def get_from_api_desk():
     Get a list of Desk cases that are assigned to Dev with the Jira Ticket IDs
     :return:  list
     """
-    result = []
-    base_url = "https://servicerocket.desk.com/api/v2/cases/search?q=status:pending%20custom_status:%22Assigned%20to%20DEV%22%20assigned:%22"
-    engineer_name = ''  # <----- Enter your name
-    engineer_name = engineer_name.split()
-    for name in engineer_name[:-1]:
-        base_url += name + '%20'
-    base_url += engineer_name[-1] + '%22'
-    request = urllib2.Request(base_url)
-    username = ''  # <---- Enter your Desk username
-    password = ''  # <------ Enter you Desk password
-    base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
-    request.add_header("Authorization", "Basic %s" % base64string)
-    json_obj = urllib2.urlopen(request)
-    data = json.load(json_obj)
-    for elem in data['_embedded']['entries']:
-        desk_id = str(elem['id'])
-        jira_id = str(elem['custom_fields']['jira_id'])
-        result.append([desk_id, jira_id])
-    return result
+    try:
+        result = []
+        base_url = "https://servicerocket.desk.com/api/v2/cases/search?q=status:pending%20custom_status:%22Assigned%20to%20DEV%22%20assigned:%22"
+        engineer_name = ''  # <----- Enter your name
+        engineer_name = engineer_name.split()
+        for name in engineer_name[:-1]:
+            base_url += name + '%20'
+        base_url += engineer_name[-1] + '%22'
+        request = urllib2.Request(base_url)
+        username = ''  # <---- Enter your Desk username
+        password = ''  # <------ Enter you Desk password
+        base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
+        request.add_header("Authorization", "Basic %s" % base64string)
+        json_obj = urllib2.urlopen(request)
+        data = json.load(json_obj)
+        for elem in data['_embedded']['entries']:
+            desk_id = str(elem['id'])
+            jira_id = str(elem['custom_fields']['jira_id'])
+            result.append([desk_id, jira_id])
+        return result
+    except:
+        print 'Could not connect to Desk. Check if usernmae and password are correct'
+        sys.exit()
 
 
 desk_info = get_from_api_desk()
